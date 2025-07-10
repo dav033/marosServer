d28f4acd6a44b71ba06dc746c02cbd58c1621c93
david.theran03@gmail.com <david.theran03@gmail.com>
2025-07-09 21:42:04 -0500

    first commit

---
+++ src/main/java/io/dav033/maroconstruction/MaroconstructionApplication.java
@@ -0,0 +1,14 @@
+package io.dav033.maroconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaroconstructionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaroconstructionApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/io/dav033/maroconstruction/MaroconstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dav033.maroconstruction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaroconstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
